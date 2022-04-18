4cd205ff957331d4a7b83261b0d507d09aca1c05
Corey Wang <corefire2004@gmail.com>
2022-04-18 17:17:35 -0400

    one liner with join method

--- make_netid.py
+++ make_netid.py
@@ -1,12 +1,3 @@
-def make_netid(name,n):
-    first_space=name.find(" ")
-    second_space=name.rfind(" ")
-    first_name=name[:first_space].lower()
-    last_name=name[second_space+1:].lower()
-    if first_space==second_space:
-        return first_name[0]+last_name[0]+str(n)
-    else:
-        middle_name = name[first_space+1:second_space].lower()
-        return first_name[0]+middle_name[0]+last_name[0]+str(n)
+def make_netid(name,n): return "".join([a[0].lower() for a in name.split()])+str(n)
 print(make_netid("Corey Wang",233))
 print(make_netid("Corey Li Wang",200))
