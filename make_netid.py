7318ec1e4ecd8484a2bb3809ec8ba19ba8df4c38
Corey Wang <corefire2004@gmail.com>
2022-04-18 17:10:45 -0400

    find

--- make_netid.py
+++ make_netid.py
@@ -1,10 +1,12 @@
 def make_netid(name,n):
-    string=""
-    a=[s[0].lower() for s in name.split()]
-    for b in a:
-        string+=b
-    string+=str(n)
-    print(string)
-
-make_netid("Corey Wang",233)
-make_netid("Corey Li Wang",200)
+    first_space=name.find(" ")
+    second_space=name.rfind(" ")
+    first_name=name[:first_space].lower()
+    last_name=name[second_space+1:].lower()
+    if first_space==second_space:
+        return first_name[0]+last_name[0]+str(n)
+    else:
+        middle_name = name[first_space+1:second_space].lower()
+        return first_name[0]+middle_name[0]+last_name[0]+str(n)
+print(make_netid("Corey Wang",233))
+print(make_netid("Corey Li Wang",200))
