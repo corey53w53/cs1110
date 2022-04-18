cdb9c2427b8fd66d3718ca7bfd9f4986f2819cc1
Corey Wang <corefire2004@gmail.com>
2022-04-18 16:59:32 -0400

    list comprehension

--- make_netid.py
+++ make_netid.py
@@ -1,9 +1,10 @@
 def make_netid(name,n):
-    nl=name.split()
-    s=""
-    for name in nl:
-        s+=name[0].lower()
-    print(s+str(n))
-    return s+str(n)
+    string=""
+    a=[s[0].lower() for s in name.split()]
+    for b in a:
+        string+=b
+    string+=str(n)
+    print(string)
+
 make_netid("Corey Wang",233)
 make_netid("Corey Li Wang",200)
