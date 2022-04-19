1d7669aa47530ff1bed374842e888fecf3c56c3b
Corey Wang <corefire2004@gmail.com>
2022-04-18 23:31:49 -0400

    idk

--- make_netid.py
+++ make_netid.py
@@ -1,3 +1,7 @@
 def make_netid(name,n): return "".join([a[0].lower() for a in name.split()])+str(n)
 print(make_netid("Corey",233))
 print(make_netid("Corey lkj sdjflk Li Wang",200980))
+
+print("he  llo".count(" "))
+
+print(type(repr("hello")))
