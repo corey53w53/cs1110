a54cd85f5f03ac50e83d3b75be99d63aa7d1b7df
Corey Wang <corefire2004@gmail.com>
2022-05-04 07:08:35 -0400

    grandchild to parent

--- inheritance.py
+++ inheritance.py
@@ -6,14 +6,29 @@ class Parent(object):
     def add_one(self):
         self.age += 1
 
+    def __str__(self):
+        return "I am parent"
+
 
 class Child(Parent):
     def __init__(self, age, gender):
-        super().__init__(age, gender)
+        super(Child, self).__init__(age, gender)
 
     def __str__(self):
         return f'I am {self.age} years old and am a {self.gender}'
 
 
+class Grandchild(Child):
+    def __init__(self, age, gender, cute):
+        super(Grandchild, self).__init__(age, gender)
+        self.cute = cute
+        pass
+
+    def __str__(self):
+        return super(Child, self).__str__()
+
+
 me = Child(4, "boy")
 print(me)
+m = Grandchild(3, "boy", True)
+print(m)
